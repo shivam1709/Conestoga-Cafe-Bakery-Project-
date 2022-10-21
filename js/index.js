41ae592f82f509a62c09c58c13509f8064f0c280
shivam1709 <68727150+shivam1709@users.noreply.github.com>
2022-10-21 18:42:16 -0400

    completed midterm

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
-function placeOrder(){  
-    alert("hello javatpoint user");  
-    }  
+
+var popup = document.getElementById("popup");
+
+var btn = document.getElementById("button");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    popup.style.display = "block";
+}
+
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+}
